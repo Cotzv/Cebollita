12bd371bbab7d03e746ad5eca93ac0c0c5487389
Sergio <serqio.kay@gmail.com>
2021-10-26 19:14:57 -0400

    add negartive test get proyect by id

---
+++ test/api/negative test/proyect.test.js
@@ -0,0 +1,22 @@
+import HttpRequestManager from "../../../src/common/api/http.request.manager";
+import errors from "../../../src/resources/errors.json";
+import endPoints from "../../../src/resources/endpoints.json";
+
+const projectsURI = endPoints.projects;
+let projectByIdURI = endPoints.projectById;
+
+describe("negative test", () => {
+    test('Verify that has the required error with a wrong ID when  GET of a proyect the request "/projects.json" endpoint is executed ', () => {
+        return HttpRequestManager.makeRequest(
+            "GET",
+            projectByIdURI.replace("{id}", "2")
+        )
+            .then(function (response) {
+                expect(response.data).toEqual(errors.ThereIsNoProyect);
+            })
+            .catch(function (error) {
+                //console.log(error);
+                throw error;
+            });
+    }, 20000);
+});
