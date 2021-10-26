05020afef38aeeefa5a906e3bf6c29a640944c64
Sergio <serqio.kay@gmail.com>
2021-10-26 19:21:54 -0400

    add a negative test with a wrong auth when get a proyect by id

--- test/api/negative test/proyect.neg.test.js
+++ test/api/negative test/proyect.neg.test.js
@@ -19,4 +19,20 @@ describe("negative test", () => {
                 throw error;
             });
     }, 20000);
+
+    test('Verify that has the required error with a wrong ID when  GET of a proyect the request "/projects.json" endpoint is executed ', () => {
+        return HttpRequestManager.makeRequest(
+            "GET",
+            projectByIdURI.replace("{id}", "3951526"),
+            null,
+            false
+        )
+            .then(function (response) {
+                expect(response.data).toEqual(errors.Authentication);
+            })
+            .catch(function (error) {
+                //console.log(error);
+                throw error;
+            });
+    }, 20000);
 });
