6948363a73bda2b8958852712d0b1ebf0afee2b3
Sergio <serqio.kay@gmail.com>
2021-10-26 20:34:01 -0400

    add a negative test that a empty name in the creation of a project

--- test/api/negative test/proyect.neg.test.js
+++ test/api/negative test/proyect.neg.test.js
@@ -1,12 +1,13 @@
 import HttpRequestManager from "../../../src/common/api/http.request.manager";
 import errors from "../../../src/resources/errors.json";
 import endPoints from "../../../src/resources/endpoints.json";
+import payloads from "../../../src/resources/payloads.json";
 
 const projectsURI = endPoints.projects;
 let projectByIdURI = endPoints.projectById;
 
 describe("negative test", () => {
-    test('Verify that has the required error with a wrong ID when  GET of a proyect the request "/projects.json" endpoint is executed ', () => {
+    test('Verify that has the required error with a wrong ID when  GET of a proyect by ID the request "/projects.json" endpoint is executed ', () => {
         return HttpRequestManager.makeRequest(
             "GET",
             projectByIdURI.replace("{id}", "2")
@@ -20,7 +21,7 @@ describe("negative test", () => {
             });
     }, 20000);
 
-    test('Verify that has the required error with a wrong ID when  GET of a proyect the request "/projects.json" endpoint is executed ', () => {
+    test('Verify that has the required error with a wrong auth when  GET of a proyect by ID the request "/projects.json" endpoint is executed ', () => {
         return HttpRequestManager.makeRequest(
             "GET",
             projectByIdURI.replace("{id}", "3951526"),
@@ -29,6 +30,21 @@ describe("negative test", () => {
         )
             .then(function (response) {
                 expect(response.data).toEqual(errors.Authentication);
+            })
+            .catch(function (error) {
+                //console.log(error);
+                throw error;
+            });
+    }, 20000);
+
+    test('Verify that has the required error with an empty name when  POST the request "/projects.json" endpoint is executed ', () => {
+        return HttpRequestManager.makeRequest(
+            "POST",
+            projectsURI,
+            payloads.ProjectById.MissingContent
+        )
+            .then(function (response) {
+                expect(response.data).toEqual(errors.ShortNameProject);
             })
             .catch(function (error) {
                 //console.log(error);
