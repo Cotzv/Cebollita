5f236193e3ff3a46b86af41ebb0df0be9b9126a2
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-10-27 06:59:09 -0400

    Adding the user tests

--- tests/api/user.test.js
+++ tests/api/user.test.js
@@ -4,15 +4,15 @@ import errors from '../../src/resources/errors.json'
 import payloads from '../../src/resources/payloads.json'
 import loggers from '../../utils/loggers.js'
 
-const projectsURI = endpoints.projects
-let projectByIdURI = endpoints.projectById
+const userURI = endpoints.user
+let userByIdURI = endpoints.userById
 let id = ''
 let postId = ''
 
-describe('Projects Tests', () => {
+describe('User Tests', () => {
     jest.setTimeout(100000);
     beforeAll(() => {
-        return HttpRequestManager.makeRequest('POST', projectsURI, payloads.ProjectById.POST)
+        return HttpRequestManager.makeRequest('POST', userURI, payloads.UserById.POST)
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
@@ -26,33 +26,33 @@ describe('Projects Tests', () => {
     })
 
     afterAll(() => {
-        return HttpRequestManager.makeRequest('DELETE', projectByIdURI.replace('{id}', postId))
+        return HttpRequestManager.makeRequest('DELETE', userByIdURI.replace('{id}', postId))
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
                 expect(response.data).not.toEqual(errors.Authentication)
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
                 throw error
             })
     })
 
-    test('Verify that a 200 OK status code result when a GET request to the "/projects.json" endpoint is executed', () => {
-        return HttpRequestManager.makeRequest('GET', projectsURI)
+    test('Verify that a 200 OK status code result when a GET request to the "/user.json" endpoint is executed', () => {
+        return HttpRequestManager.makeRequest('GET', userURI)
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
                 expect(response.data).not.toEqual(errors.Authentication)
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
                 throw error
             })
     })
 
-    test('Verify that a 200 OK status code result when a POST request to the "/projects.json" endpoint is executed', () => {
-        return HttpRequestManager.makeRequest('POST', projectsURI, payloads.ProjectById.POST)
+    test('Verify that a 200 OK status code result when a POST request to the "/user.json" endpoint is executed', () => {
+        return HttpRequestManager.makeRequest('POST', userURI, payloads.UserById.POST)
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
@@ -60,46 +60,70 @@ describe('Projects Tests', () => {
                 postId = response.data.Id
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
                 throw error
             })
     })
 
-    test('Verify that a 200 OK status code result when a PUT request to the "/projects/{id}.json" endpoint is executed', () => {
-        return HttpRequestManager.makeRequest('PUT', projectByIdURI.replace('{id}', id), payloads.ProjectById.PUT)
+    test('Verify that a 200 OK status code result when a PUT request to the "/user/{id}.json" endpoint is executed', () => {
+        return HttpRequestManager.makeRequest('PUT', userByIdURI.replace('{id}', id), payloads.UserById.PUT)
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
                 expect(response.data).not.toEqual(errors.Authentication)
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
                 throw error
             })
     })
 
-    test('Verify that a 200 OK status code result when a DELETE request to the "/projects/{id}.json" endpoint is executed', () => {
-        return HttpRequestManager.makeRequest('DELETE', projectByIdURI.replace('{id}', id))
+    test('Verify that a 200 OK status code result when a DELETE request to the "/user/{id}.json" endpoint is executed', () => {
+        return HttpRequestManager.makeRequest('DELETE', userByIdURI.replace('{id}', id))
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
                 expect(response.data).not.toEqual(errors.Authentication)
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
                 throw error
             })
     })
 
-    test('Verify that an error response result when a GET request to the "/projects.json" endpoint is executed with invalid credentials', () => {
-        return HttpRequestManager.makeRequest('GET', projectsURI, '', false)
+    test('Verify that an error response result when a GET request to the "/user.json" endpoint is executed with invalid credentials', () => {
+        return HttpRequestManager.makeRequest('GET', userURI, '', false)
             .then(function (response) {
                 expect(response.status).toBe(200)
                 expect(response.statusText).toMatch('OK')
                 expect(response.data).toEqual(errors.Authentication)
             })
             .catch(function (error) {
-                console.log(error)
+                loggers.error(error)
+                throw error
+            })
+    })
+    test('Verify that an error response result when a GET request to the "/user.json" endpoint is executed with invalid email address', () => {
+        return HttpRequestManager.makeRequest('GET', userURI, '', false)
+            .then(function (response) {
+                expect(response.status).toBe(200)
+                expect(response.statusText).toMatch('OK')
+                expect(response.data).toEqual(errors.InvalidEmail)
+            })
+            .catch(function (error) {
+                loggers.error(error)
+                throw error
+            })
+    })
+    test('Verify that an error response result when a GET request to the "/user.json" endpoint is executed with invalid email address', () => {
+        return HttpRequestManager.makeRequest('GET', userURI, '', false)
+            .then(function (response) {
+                expect(response.status).toBe(200)
+                expect(response.statusText).toMatch('OK')
+                expect(response.data).toEqual(errors.InvalidEmail)
+            })
+            .catch(function (error) {
+                loggers.error(error)
                 throw error
             })
     })
